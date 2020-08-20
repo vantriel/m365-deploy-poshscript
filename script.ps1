eb600896142f7acbae9ceeddf76c169a1c2fe11c
vantriel <julian.vantriel@protonmail.com>
2020-08-20 02:00:53 +0200

    added script.ps1 and config-fallback.xml

---
+++ script.ps1
@@ -0,0 +1,13 @@
+﻿$msODTDownloadUri="https://www.microsoft.com/en-us/download/details.aspx?id=49117"
+$fallbackConfig="config-fallback.xml"
+
+function Find-ODTDownloadURL(){
+    try{
+        $msDownloadPage = Invoke-WebRequest -Uri $msODTDownloadUri
+        $msDownloadPage.Where
+    }
+    catch{
+    }
+    finally{
+    }
+}
